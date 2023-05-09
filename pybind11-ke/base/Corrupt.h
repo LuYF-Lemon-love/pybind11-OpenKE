9b533d1404a7a4c3c6805c0ceb92239ca911093d
LuYF-Lemon-love <3555028709@qq.com>
2023-05-09 15:27:05 +0800

    提交代码

--- pybind11-ke/base/Corrupt.h
+++ pybind11-ke/base/Corrupt.h
@@ -1,7 +1,7 @@
 // pybind11-ke/base/Corrupt.h
 // 
 // git pull from OpenKE-PyTorch by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 7, 2023
-// updated by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 8, 2023
+// updated by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 9, 2023
 // 
 // 该头文件定义了破坏三元组的方法.
 
@@ -11,8 +11,12 @@
 #include "Triple.h"
 #include "Reader.h"
 
+// 用 head 和 relation 构建负三元组，即替换 tail
+// 该函数返回负三元组的 tail
 INT corrupt_head(INT id, INT h, INT r, bool filter_flag = true) {
 	INT lef, rig, mid, ll, rr;
+
+	// 如果不过滤
 	if (not filter_flag) {
 		INT tmp = rand_max(id, entityTotal - 1);
 		if (tmp < h)
@@ -20,33 +24,52 @@ INT corrupt_head(INT id, INT h, INT r, bool filter_flag = true) {
 		else
 			return tmp + 1;
 	}
+
+	// lef: head(h) 在 trainHead 中第一次出现的前一个位置
+	// rig: head(h) 在 trainHead 中最后一次出现的位置
 	lef = lefHead[h] - 1;
 	rig = rigHead[h];
 	while (lef + 1 < rig) {
 		mid = (lef + rig) >> 1;
+		// 二分查找算法变体
+		// 由于 >= -> rig，所以 rig 最终在第一个 r 的位置
 		if (trainHead[mid].r >= r) rig = mid; else
 		lef = mid;
 	}
 	ll = rig;
+
 	lef = lefHead[h];
 	rig = rigHead[h] + 1;
 	while (lef + 1 < rig) {
 		mid = (lef + rig) >> 1;
+		// 二分查找算法变体
+		// 由于 <= -> lef，所以 lef 最终在最后一个 r 的位置
 		if (trainHead[mid].r <= r) lef = mid; else
 		rig = mid;
 	}
 	rr = lef;
+
+	// 只能产生 (entity_total - (rr - ll + 1)) 种实体，即去掉训练集中已有的三元组
 	INT tmp = rand_max(id, entityTotal - (rr - ll + 1));
+
+	// 第一种：tmp 小于第一个 r 对应的 tail
 	if (tmp < trainHead[ll].t) return tmp;
+
+	// 第二种：tmp 大于最后一个 r 对应的 tail
 	if (tmp > trainHead[rr].t - rr + ll - 1) return tmp + rr - ll + 1;
+
+	// 第三种：由于 (>= -> rig), (lef + 1 < rig), (tmp + lef - ll + 1)
+	// 因此最终返回取值为 (train_head[lef].t, train_head[rig].t) 的 tail
 	lef = ll, rig = rr + 1;
 	while (lef + 1 < rig) {
 		mid = (lef + rig) >> 1;
+		// 类似 tmp > trainHead[rr].t - rr + ll - 1
 		if (trainHead[mid].t - mid + ll - 1 < tmp)
 			lef = mid;
 		else 
 			rig = mid;
 	}
+	// 类似 tmp + rr - ll + 1
 	return tmp + lef - ll + 1;
 }
 
