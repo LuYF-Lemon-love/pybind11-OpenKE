511811ceff66824594c9d2e8c2b43463061e90cd
LuYF-Lemon-love <3555028709@qq.com>
2023-05-10 14:30:55 +0800

    提交代码

--- pybind11-ke/base/Corrupt.h
+++ pybind11-ke/base/Corrupt.h
@@ -135,9 +135,12 @@ INT corrupt_tail(INT id, INT t, INT r, bool filter_flag = true) {
 	return tmp + lef - ll + 1;
 }
 
-
+// 用 head 和 tail 构建负三元组，即替换 relation
+// 该函数返回负三元组的 relation
 INT corrupt_rel(INT id, INT h, INT t, INT r, bool p = false, bool filter_flag = true) {
 	INT lef, rig, mid, ll, rr;
+
+	// 如果不过滤
 	if (not filter_flag) {
 		INT tmp = rand_max(id, relationTotal - 1);
 		if (tmp < r)
@@ -145,27 +148,38 @@ INT corrupt_rel(INT id, INT h, INT t, INT r, bool p = false, bool filter_flag = 
 		else
 			return tmp + 1;
 	}
+
+	// lef: head(h) 在 trainRel 中第一次出现的前一个位置
+	// rig: head(h) 在 trainRel 中最后一次出现的位置
 	lef = lefRel[h] - 1;
 	rig = rigRel[h];
 	while (lef + 1 < rig) {
 		mid = (lef + rig) >> 1;
+		// 二分查找算法变体
+		// 由于 >= -> rig，所以 rig 最终在第一个 t 的位置
 		if (trainRel[mid].t >= t) rig = mid; else
 		lef = mid;
 	}
 	ll = rig;
+
 	lef = lefRel[h];
 	rig = rigRel[h] + 1;
 	while (lef + 1 < rig) {
 		mid = (lef + rig) >> 1;
+		// 二分查找算法变体
+		// 由于 <= -> lef，所以 lef 最终在最后一个 t 的位置
 		if (trainRel[mid].t <= t) lef = mid; else
 		rig = mid;
 	}
 	rr = lef;
+
 	INT tmp;
 	if(p == false) {	
+		// 只能产生 (elationTotal - (rr - ll + 1)) 种关系，即去掉训练集中已有的三元组
 		tmp = rand_max(id, relationTotal - (rr - ll + 1));
 	}
 	else {
+		// 利用概率分布替换 relation
 		INT start = r * (relationTotal - 1);
 		REAL sum = 1;
 		bool *record = (bool *)calloc(relationTotal - 1, sizeof(bool));
@@ -202,16 +216,25 @@ INT corrupt_rel(INT id, INT h, INT t, INT r, bool p = false, bool filter_flag = 
 		free(prob_tmp);
 		free(record);
 	}
+
+	// 第一种：tmp 小于第一个 t 对应的 relation
 	if (tmp < trainRel[ll].r) return tmp;
+
+	// 第二种：tmp 大于最后一个 t 对应的 relation
 	if (tmp > trainRel[rr].r - rr + ll - 1) return tmp + rr - ll + 1;
+	
+	// 第三种：由于 (>= -> rig), (lef + 1 < rig), (tmp + lef - ll + 1)
+	// 因此最终返回取值为 (train_head[lef].t, train_head[rig].t) 的 tail
 	lef = ll, rig = rr + 1;
 	while (lef + 1 < rig) {
 		mid = (lef + rig) >> 1;
+		// 类似 tmp > trainRel[rr].r - rr + ll - 1
 		if (trainRel[mid].r - mid + ll - 1 < tmp)
 			lef = mid;
 		else 
 			rig = mid;
 	}
+	// 类似 tmp + rr - ll + 1
 	return tmp + lef - ll + 1;
 }
 
