bbb79a63ca993af66c45eae9484297a057c5a20e
LuYF-Lemon-love <3555028709@qq.com>
2023-05-10 17:40:23 +0800

    提交代码

--- pybind11-ke/module/BaseModule.py
+++ pybind11-ke/module/BaseModule.py
@@ -1,3 +1,10 @@
+# pybind11-ke/module/BaseModule.py
+# 
+# git pull from OpenKE-PyTorch by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 7, 2023
+# updated by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 10, 2023
+# 
+# 该头文件定义了 BaseModule.
+
 import torch
 import torch.nn as nn
 import os
