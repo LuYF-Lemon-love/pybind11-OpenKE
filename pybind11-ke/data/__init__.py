5db6744050da68932e683813fd6f0d728fcded0b
LuYF-Lemon-love <3555028709@qq.com>
2023-05-10 17:19:21 +0800

    提交代码

--- pybind11-ke/data/__init__.py
+++ pybind11-ke/data/__init__.py
@@ -1,3 +1,12 @@
+# coding:utf-8
+#
+# pybind11-ke/data/__init__.py
+#
+# git pull from OpenKE-PyTorch by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 7, 2023
+# updated by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 10, 2023
+#
+# 该脚本定义了接口.
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
