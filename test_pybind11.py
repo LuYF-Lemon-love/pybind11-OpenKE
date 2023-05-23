d941219cbd2369f5a1af41e8f2aaa17ac204ca68
LuYF-Lemon-love <3555028709@qq.com>
2023-05-23 17:53:14 +0800

    开始利用pybind11修改代码

--- test_pybind11.py
+++ test_pybind11.py
@@ -18,8 +18,8 @@ from pybind11_ke.data import TrainDataLoader, TestDataLoader
 # dataloader for training
 train_dataloader = TrainDataLoader(
 	in_path = "./benchmarks/FB15K237/", 
-	nbatches = 5,
-	threads = 32, 
+	nbatches = 100,
+	threads = 8, 
 	sampling_mode = "normal", 
 	bern_flag = 1, 
 	filter_flag = 1, 
@@ -39,16 +39,16 @@ transe = TransE(
 
 
 # define the loss function
-# model = NegativeSampling(
-# 	model = transe, 
-# 	loss = MarginLoss(margin = 5.0),
-# 	batch_size = train_dataloader.get_batch_size()
-# )
+model = NegativeSampling(
+	model = transe, 
+	loss = MarginLoss(margin = 5.0),
+	batch_size = train_dataloader.get_batch_size()
+)
 
 # train the model
-# trainer = Trainer(model = model, data_loader = train_dataloader, train_times = 1000, alpha = 1.0, use_gpu = True)
-# trainer.run()
-# transe.save_checkpoint('./checkpoint/transe.ckpt')
+trainer = Trainer(model = model, data_loader = train_dataloader, train_times = 1000, alpha = 1.0, use_gpu = True)
+trainer.run()
+transe.save_checkpoint('./checkpoint/transe.ckpt')
 
 # test the model
 transe.load_checkpoint('./checkpoint/transe.ckpt')
