dc48b06457d2e46597dd01f626f7caee7dada46f
LuYF-Lemon-love <3555028709@qq.com>
2023-05-24 16:29:24 +0800

    开始利用pybind11修改代码

--- pybind11_ke/data/TestDataLoader.py
+++ pybind11_ke/data/TestDataLoader.py
@@ -8,7 +8,6 @@
 # 该脚本定义了采样数据的函数, 用于验证模型.
 
 import os
-# import ctypes
 import numpy as np
 from ..release import base
 
@@ -35,30 +34,6 @@ class TestDataSampler(object):
 class TestDataLoader(object):
 
 	def __init__(self, in_path = "./", sampling_mode = 'link', type_constrain = True):
-		# base_file = os.path.abspath(os.path.join(os.path.dirname(__file__), "../release/Base.so"))
-		# 导入 C 语言的动态库
-		# self.lib = ctypes.cdll.LoadLibrary(base_file)
-		# 指定函数原型
-		"""for link prediction"""
-		# self.lib.getHeadBatch.argtypes = [
-		# 	ctypes.c_void_p,
-		# 	ctypes.c_void_p,
-		# 	ctypes.c_void_p,
-		# ]
-		# self.lib.getTailBatch.argtypes = [
-		# 	ctypes.c_void_p,
-		# 	ctypes.c_void_p,
-		# 	ctypes.c_void_p,
-		# ]
-		"""for triple classification"""
-		# self.lib.getTestBatch.argtypes = [
-		# 	ctypes.c_void_p,
-		# 	ctypes.c_void_p,
-		# 	ctypes.c_void_p,
-		# 	ctypes.c_void_p,
-		# 	ctypes.c_void_p,
-		# 	ctypes.c_void_p,
-		# ]
 		"""set essential parameters"""
 		self.in_path = in_path
 		self.sampling_mode = sampling_mode
@@ -67,49 +42,33 @@ class TestDataLoader(object):
 		self.read()
 
 	def read(self):
-		# self.lib.setInPath(ctypes.create_string_buffer(self.in_path.encode(), len(self.in_path) * 2))
 		base.setInPath(self.in_path)
-		# self.lib.randReset()
 		base.randReset()
-		# self.lib.importTestFiles()
 		base.importTestFiles()
 
 		if self.type_constrain:
-			# self.lib.importTypeFiles()
 			base.importTypeFiles()
 
-		# self.relTotal = self.lib.getRelationTotal()
 		self.relTotal = base.getRelationTotal()
-		# self.entTotal = self.lib.getEntityTotal()
 		self.entTotal = base.getEntityTotal()
-		# self.testTotal = self.lib.getTestTotal()
 		self.testTotal = base.getTestTotal()
 
 		# 利用 np.zeros 分配内存
 		self.test_h = np.zeros(self.entTotal, dtype=np.int64)
 		self.test_t = np.zeros(self.entTotal, dtype=np.int64)
 		self.test_r = np.zeros(self.entTotal, dtype=np.int64)
-		# self.test_h_addr = self.test_h.__array_interface__["data"][0]
-		# self.test_t_addr = self.test_t.__array_interface__["data"][0]
-		# self.test_r_addr = self.test_r.__array_interface__["data"][0]
 
 		self.test_pos_h = np.zeros(self.testTotal, dtype=np.int64)
 		self.test_pos_t = np.zeros(self.testTotal, dtype=np.int64)
 		self.test_pos_r = np.zeros(self.testTotal, dtype=np.int64)
-		# self.test_pos_h_addr = self.test_pos_h.__array_interface__["data"][0]
-		# self.test_pos_t_addr = self.test_pos_t.__array_interface__["data"][0]
-		# self.test_pos_r_addr = self.test_pos_r.__array_interface__["data"][0]
+		
 		self.test_neg_h = np.zeros(self.testTotal, dtype=np.int64)
 		self.test_neg_t = np.zeros(self.testTotal, dtype=np.int64)
 		self.test_neg_r = np.zeros(self.testTotal, dtype=np.int64)
-		# self.test_neg_h_addr = self.test_neg_h.__array_interface__["data"][0]
-		# self.test_neg_t_addr = self.test_neg_t.__array_interface__["data"][0]
-		# self.test_neg_r_addr = self.test_neg_r.__array_interface__["data"][0]
 
 	# 为链接预测进行采样数据
 	def sampling_lp(self):
 		res = []
-		# self.lib.getHeadBatch(self.test_h_addr, self.test_t_addr, self.test_r_addr)
 		base.getHeadBatch(self.test_h, self.test_t, self.test_r)
 		res.append({
 			"batch_h": self.test_h.copy(), 
@@ -117,7 +76,6 @@ class TestDataLoader(object):
 			"batch_r": self.test_r[:1].copy(),
 			"mode": "head_batch"
 		})
-		# self.lib.getTailBatch(self.test_h_addr, self.test_t_addr, self.test_r_addr)
 		base.getTailBatch(self.test_h, self.test_t, self.test_r)
 		res.append({
 			"batch_h": self.test_h[:1],
