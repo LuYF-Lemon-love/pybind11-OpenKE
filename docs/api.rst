3507d89180e58df2f9c4d885dea60c817385e219
LuYF-Lemon-love <3555028709@qq.com>
2023-05-26 16:39:10 +0800

    开始利用pybind11修改代码

--- docs/api.rst
+++ docs/api.rst
@@ -5,4 +5,4 @@ API
    :toctree: generated
    :recursive:
 
-   module
+   base
--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../pybind11_ke'))
+print(os.path.abspath('../pybind11_ke'))
 
 
 # -- Project information -----------------------------------------------------
