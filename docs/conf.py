5e09237281a77404640a2e3a855c32b7c15cd091
LuYF-Lemon-love <3555028709@qq.com>
2023-05-26 15:57:00 +0800

    开始利用pybind11修改代码

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "Pybind11-OpenKE"
+copyright = "2023, LuYF-Lemon-love"
+author = "LuYF-Lemon-love"
 
 
 # -- General configuration ---------------------------------------------------
