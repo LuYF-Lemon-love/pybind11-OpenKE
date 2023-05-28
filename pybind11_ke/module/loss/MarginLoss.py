53603efaea35e1505addeb14c7a7ef8815a40e0e
LuYF-Lemon-love <3555028709@qq.com>
2023-05-28 21:10:33 +0800

    开始利用pybind11修改代码

--- pybind11_ke/module/loss/MarginLoss.py
+++ pybind11_ke/module/loss/MarginLoss.py
@@ -1,8 +1,33 @@
+# coding:utf-8
+#
+# pybind11_ke/module/loss/MarginLoss.py
+#
+# git pull from OpenKE-PyTorch by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 7, 2023
+# updated by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 28, 2023
+#
+# 该脚本定义了 margin-based ranking criterion 损失函数.
+
+"""
+MarginLoss - 损失函数类，TransE 原论文中应用这种损失函数完成模型学习。
+
+基本用法如下：
+
+.. code-block:: python
+
+	from pybind11_ke.module.loss import MarginLoss
+	from pybind11_ke.module.strategy import NegativeSampling
+	
+	# define the loss function
+	model = NegativeSampling(
+		model = transe, 
+		loss = MarginLoss(margin = 5.0),
+		batch_size = train_dataloader.get_batch_size()
+	)
+"""
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
-import numpy as np
 from .Loss import Loss
 
 class MarginLoss(Loss):
