b4374c1f4ae55f4d3ae8cda3330c62d6f4359ef6
LuYF-Lemon-love <3555028709@qq.com>
2023-05-30 21:23:47 +0800

    开始利用pybind11修改代码

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # Available at setup time due to pyproject.toml
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
+from setuptools import find_packages  # or find_namespace_packages
 
 __version__ = "0.0.1"
 
@@ -12,13 +13,28 @@ ext_modules = [
         ),
 ]
 
+# setup(
+#     name="base",
+#     version=__version__,
+#     author="LuYF-Lemon-love",
+#     author_email="3555028709@qq.com",
+#     url="https://github.com/LuYF-Lemon-love/pybind11-OpenKE",
+#     description="pybind11-OpenKE 的底层数据处理模块",
+#     long_description="",
+#     ext_modules=ext_modules,
+#     extras_require={"test": "pytest"},
+#     cmdclass={"build_ext": build_ext},
+#     zip_safe=False,
+#     python_requires=">=3.7",
+# )
+
 setup(
-    name="base",
+    name="pybind11_ke",
     version=__version__,
     author="LuYF-Lemon-love",
     author_email="3555028709@qq.com",
     url="https://github.com/LuYF-Lemon-love/pybind11-OpenKE",
-    description="pybind11-OpenKE 的底层数据处理模块",
+    description="知识图谱嵌入学习包",
     long_description="",
     ext_modules=ext_modules,
     extras_require={"test": "pytest"},
