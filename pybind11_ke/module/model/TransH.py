43729638ff3dc6c98ea8edfcc29c528d52cc03f8
LuYF-Lemon-love <3555028709@qq.com>
2023-06-02 18:20:39 +0800

    开始利用pybind11修改代码

--- pybind11_ke/module/model/TransH.py
+++ pybind11_ke/module/model/TransH.py
@@ -1,3 +1,54 @@
+# coding:utf-8
+#
+# pybind11_ke/module/model/TransH.py
+# 
+# git pull from OpenKE-PyTorch by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 7, 2023
+# updated by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on June 2, 2023
+# 
+# 该头文件定义了 TransH.
+
+"""
+TransH - 是第二个平移模型，将关系建模为超平面上的平移操作。
+
+论文地址: `Knowledge Graph Embedding by Translating on Hyperplanes <https://ojs.aaai.org/index.php/AAAI/article/view/8870>`__ 。
+
+基本用法如下：
+
+.. code-block:: python
+
+	from pybind11_ke.config import Trainer, Tester
+	from pybind11_ke.module.model import TransH
+	from pybind11_ke.module.loss import MarginLoss
+	from pybind11_ke.module.strategy import NegativeSampling
+
+	# define the model
+	transh = TransH(
+		ent_tot = train_dataloader.get_ent_tot(),
+		rel_tot = train_dataloader.get_rel_tot(),
+		dim = 200, 
+		p_norm = 1, 
+		norm_flag = True)
+
+	# define the loss function
+	model = NegativeSampling(
+		model = transh, 
+		loss = MarginLoss(margin = 4.0),
+		batch_size = train_dataloader.get_batch_size()
+	)
+
+
+	# train the model
+	trainer = Trainer(model = model, data_loader = train_dataloader,
+		train_times = 1000, alpha = 0.5, use_gpu = True)
+	trainer.run()
+	transh.save_checkpoint('../checkpoint/transh.ckpt')
+
+	# test the model
+	transh.load_checkpoint('../checkpoint/transh.ckpt')
+	tester = Tester(model = transh, data_loader = test_dataloader, use_gpu = True)
+	tester.run_link_prediction(type_constrain = False)
+"""
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -5,17 +56,53 @@ from .Model import Model
 
 class TransH(Model):
 
-	def __init__(self, ent_tot, rel_tot, dim = 100, p_norm = 1, norm_flag = True, margin = None, epsilon = None):
+	"""
+	TransH 类，继承自 :py:class:`pybind11_ke.module.model.Model`。
+	
+	TransH 提出于 2014 年，是第二个平移模型，将关系建模为超平面上的平移操作。
+	"""
+
+	def __init__(self, ent_tot, rel_tot, dim = 100, p_norm = 1,
+	      norm_flag = True, margin = None, epsilon = None):
+		
+		"""创建 TransH 对象。
+
+		:param ent_tot: 实体的个数
+		:type ent_tot: int
+		:param rel_tot: 关系的个数
+		:type rel_tot: int
+		:param dim: 实体、关系嵌入向量和和法向量的维度
+		:type dim: int
+		:param p_norm: 评分函数的距离函数, 按照原论文，这里可以取 1 或 2。
+		:type p_norm: int
+		:param norm_flag: 是否利用 :py:func:`torch.nn.functional.normalize` 
+						  对实体和关系嵌入的最后一维执行 L2-norm。
+		:type norm_flag: bool
+		:param margin: 原论文中损失函数的 gamma。
+		:type margin: float
+		:param epsilon: 对于 TransH 没什么用
+		:type epsilon: float
+		"""
+
 		super(TransH, self).__init__(ent_tot, rel_tot)
 		
+		#: 实体、关系嵌入向量和和法向量的维度
 		self.dim = dim
-		self.margin = margin
-		self.epsilon = epsilon
-		self.norm_flag = norm_flag
+		#: 评分函数的距离函数, 按照原论文，这里可以取 1 或 2。
 		self.p_norm = p_norm
-
+		#: 是否利用 :py:func:`torch.nn.functional.normalize` 
+		#: 对实体和关系嵌入向量的最后一维执行 L2-norm。
+		self.norm_flag = norm_flag
+		#: 原论文中损失函数的 gamma。
+		self.margin = margin
+		#: 对于 TransH 没什么用
+		self.epsilon = epsilon
+		
+		#: 根据实体个数，创建的实体嵌入
 		self.ent_embeddings = nn.Embedding(self.ent_tot, self.dim)
+		#: 根据关系个数，创建的关系嵌入
 		self.rel_embeddings = nn.Embedding(self.rel_tot, self.dim)
+		#: 根据关系个数，创建的法向量
 		self.norm_vector = nn.Embedding(self.rel_tot, self.dim)
 
 		if margin == None or epsilon == None:
@@ -45,27 +132,62 @@ class TransH(Model):
 		if margin != None:
 			self.margin = nn.Parameter(torch.Tensor([margin]))
 			self.margin.requires_grad = False
+			#: :py:attr:`margin` 是否为 None。
 			self.margin_flag = True
 		else:
 			self.margin_flag = False
 
 	def _calc(self, h, t, r, mode):
+		"""计算 TransH 的评分函数。
+		
+		:param h: 头实体的向量。
+		:type h: torch.Tensor
+		:param t: 尾实体的向量。
+		:type t: torch.Tensor
+		:param r: 关系实体的向量。
+		:type r: torch.Tensor
+		:param mode: 如果进行链接预测的话：``normal`` 表示 :py:class:`pybind11_ke.data.TrainDataLoader` 
+					 为训练进行采样的数据，``head_batch`` 和 ``tail_batch`` 
+					 表示 :py:class:`pybind11_ke.data.TestDataLoader` 为验证模型采样的数据。
+		:type mode: str
+		:returns: 三元组的得分
+		:rtype: torch.Tensor
+		"""
+
+		# 对嵌入的最后一维进行归一化
 		if self.norm_flag:
 			h = F.normalize(h, 2, -1)
 			r = F.normalize(r, 2, -1)
 			t = F.normalize(t, 2, -1)
+		
+		# 保证 h, r, t 都是三维的
 		if mode != 'normal':
 			h = h.view(-1, r.shape[0], h.shape[-1])
 			t = t.view(-1, r.shape[0], t.shape[-1])
 			r = r.view(-1, r.shape[0], r.shape[-1])
+		
+		# 两者结果一样，括号只是逻辑上的，'head_batch' 是替换 head，否则替换 tail
 		if mode == 'head_batch':
 			score = h + (r - t)
 		else:
 			score = (h + r) - t
+		
+		# 利用距离函数计算得分
 		score = torch.norm(score, self.p_norm, -1).flatten()
 		return score
 
 	def _transfer(self, e, norm):
+		"""
+		将头实体或尾实体的向量投影到超平面上。
+		
+		:param e: 头实体或尾实体向量。
+		:type e: torch.Tensor
+		:param norm: 法向量
+		:type norm: torch.Tensor
+		:returns: 投影后的实体向量
+		:rtype: torch.Tensor
+		"""
+
 		norm = F.normalize(norm, p = 2, dim = -1)
 		if e.shape[0] != norm.shape[0]:
 			e = e.view(-1, norm.shape[0], e.shape[-1])
@@ -76,6 +198,16 @@ class TransH(Model):
 			return e - torch.sum(e * norm, -1, True) * norm
 
 	def forward(self, data):
+		"""
+		定义每次调用时执行的计算。
+		:py:class:`torch.nn.Module` 子类必须重写 :py:meth:`torch.nn.Module.forward`。
+		
+		:param data: 数据。
+		:type data: dict
+		:returns: 三元组的得分
+		:rtype: torch.Tensor
+		"""
+
 		batch_h = data['batch_h']
 		batch_t = data['batch_t']
 		batch_r = data['batch_r']
@@ -93,6 +225,14 @@ class TransH(Model):
 			return score
 
 	def regularization(self, data):
+		"""L2 正则化函数（又称权重衰减），在损失函数中用到。
+		
+		:param data: 数据。
+		:type data: dict
+		:returns: 模型参数的正则损失
+		:rtype: torch.Tensor
+		"""
+
 		batch_h = data['batch_h']
 		batch_t = data['batch_t']
 		batch_r = data['batch_r']
@@ -107,6 +247,14 @@ class TransH(Model):
 		return regul
 	
 	def predict(self, data):
+		"""TransE 的推理方法。
+		
+		:param data: 数据。
+		:type data: dict
+		:returns: 三元组的得分
+		:rtype: numpy.ndarray
+		"""
+		
 		score = self.forward(data)
 		if self.margin_flag:
 			score = self.margin - score
