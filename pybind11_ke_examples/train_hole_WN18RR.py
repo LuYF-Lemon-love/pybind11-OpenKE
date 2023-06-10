d6bc5dd4eb1285347d574882380d553c423335b5
LuYF-Lemon-love <3555028709@qq.com>
2023-06-10 18:16:00 +0800

    开始利用pybind11修改代码

--- pybind11_ke_examples/train_hole_WN18RR.py
+++ pybind11_ke_examples/train_hole_WN18RR.py
@@ -98,7 +98,8 @@ model = NegativeSampling(
 # 可以运行它的 :py:meth:`pybind11_ke.config.Trainer.run` 函数进行模型学习。
 
 # train the model
-trainer = Trainer(model = model, data_loader = train_dataloader, train_times = 1000, alpha = 0.5, use_gpu = True, opt_method = "adagrad")
+trainer = Trainer(model = model, data_loader = train_dataloader,
+                  train_times = 1000, alpha = 0.5, use_gpu = True, opt_method = "adagrad")
 trainer.run()
 hole.save_checkpoint('../checkpoint/hole.ckpt')
 
