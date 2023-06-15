c2be29dbd07118e43a995a40b95c87e927402288
LuYF-Lemon-love <3555028709@qq.com>
2023-06-15 14:55:29 +0800

    开始利用pybind11修改代码

--- pybind11_ke/module/model/ComplEx.py
+++ pybind11_ke/module/model/ComplEx.py
@@ -63,11 +63,11 @@ class ComplEx(Model):
 	评分函数为:
 
     .. math::
-    
+        
         f(\mathbf{h},\mathbf{r},\mathbf{t}) &= <\operatorname{Re}(\mathbf{r}), \operatorname{Re}(\mathbf{h}), \operatorname{Re}(\mathbf{t})> \\
-                                            &+ <\operatorname{Re}(\mathbf{r}), \operatorname{Im}(\mathbf{h}), \operatorname{Im}(\mathbf{t})> \\
-                                            &+ <\operatorname{Im}(\mathbf{r}), \operatorname{Re}(\mathbf{h}), \operatorname{Im}(\mathbf{t})> \\
-                                            &- <\operatorname{Im}(\mathbf{r}), \operatorname{Im}(\mathbf{h}), \operatorname{Re}(\mathbf{t})>
+        &+ <\operatorname{Re}(\mathbf{r}), \operatorname{Im}(\mathbf{h}), \operatorname{Im}(\mathbf{t})> \\
+        &+ <\operatorname{Im}(\mathbf{r}), \operatorname{Re}(\mathbf{h}), \operatorname{Im}(\mathbf{t})> \\
+        &- <\operatorname{Im}(\mathbf{r}), \operatorname{Im}(\mathbf{h}), \operatorname{Re}(\mathbf{t})>
         
 	:math:`< \mathbf{a}, \mathbf{b}, \mathbf{c} >` 为逐元素多线性点积（element-wise multi-linear dot product），
 	正三元组的评分函数的值越大越好，负三元组越小越好。
