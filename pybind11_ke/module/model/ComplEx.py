26981d4829238d0f8ee0ce6a0f3879e11d5f63dc
LuYF-Lemon-love <3555028709@qq.com>
2023-06-15 14:59:06 +0800

    开始利用pybind11修改代码

--- pybind11_ke/module/model/ComplEx.py
+++ pybind11_ke/module/model/ComplEx.py
@@ -60,14 +60,7 @@ class ComplEx(Model):
 	ComplEx 提出于 2016 年，第一个真正意义上复数域模型，简单而且高效。复数版本的 
         :py:class:`pybind11_ke.module.model.DistMult`。
 
-	评分函数为:
-
-    .. math::
-        
-        f(\mathbf{h},\mathbf{r},\mathbf{t}) &= <\operatorname{Re}(\mathbf{r}), \operatorname{Re}(\mathbf{h}), \operatorname{Re}(\mathbf{t})> \\
-        &+ <\operatorname{Re}(\mathbf{r}), \operatorname{Im}(\mathbf{h}), \operatorname{Im}(\mathbf{t})> \\
-        &+ <\operatorname{Im}(\mathbf{r}), \operatorname{Re}(\mathbf{h}), \operatorname{Im}(\mathbf{t})> \\
-        &- <\operatorname{Im}(\mathbf{r}), \operatorname{Im}(\mathbf{h}), \operatorname{Re}(\mathbf{t})>
+	评分函数为: :math:`<\operatorname{Re}(\mathbf{r}), \operatorname{Re}(\mathbf{h}), \operatorname{Re}(\mathbf{t})> + <\operatorname{Re}(\mathbf{r}), \operatorname{Im}(\mathbf{h}), \operatorname{Im}(\mathbf{t})> + <\operatorname{Im}(\mathbf{r}), \operatorname{Re}(\mathbf{h}), \operatorname{Im}(\mathbf{t})> - <\operatorname{Im}(\mathbf{r}), \operatorname{Im}(\mathbf{h}), \operatorname{Re}(\mathbf{t})>`
         
 	:math:`< \mathbf{a}, \mathbf{b}, \mathbf{c} >` 为逐元素多线性点积（element-wise multi-linear dot product），
 	正三元组的评分函数的值越大越好，负三元组越小越好。
