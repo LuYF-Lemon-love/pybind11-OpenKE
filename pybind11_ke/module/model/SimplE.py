2fad52d95f05b4cf39459f18a5d2bb7b92da5afb
LuYF-Lemon-love <3555028709@qq.com>
2023-06-27 20:13:32 +0800

    修复SimplE代码

--- pybind11_ke/module/model/SimplE.py
+++ pybind11_ke/module/model/SimplE.py
@@ -55,13 +55,13 @@ from .Model import Model
 class SimplE(Model):
     
     """
-	:py:class:`SimplE` 类，继承自 :py:class:`pybind11_ke.module.model.Model`。
-	
-	SimplE 提出于 2018 年，简单的双线性模型，能够为头实体和尾实体学习不同的嵌入向量。
-
-	评分函数为: :math:`1/2(<\mathbf{h}_{i}, \mathbf{v}_r, \mathbf{t}_{j}> + <\mathbf{h}_{j}, \mathbf{v}_{r^{-1}}, \mathbf{t}_{i}>)`，
+    :py:class:`SimplE` 类，继承自 :py:class:`pybind11_ke.module.model.Model`。
+    
+    SimplE 提出于 2018 年，简单的双线性模型，能够为头实体和尾实体学习不同的嵌入向量。
+    
+    评分函数为: :math:`1/2(<\mathbf{h}_{i}, \mathbf{v}_r, \mathbf{t}_{j}> + <\mathbf{h}_{j}, \mathbf{v}_{r^{-1}}, \mathbf{t}_{i}>)`，
         :math:`< \mathbf{a}, \mathbf{b}, \mathbf{c} >` 为逐元素多线性点积（element-wise multi-linear dot product），
-	正三元组的评分函数的值越大越好，负三元组越小越好。
+    正三元组的评分函数的值越大越好，负三元组越小越好。
 	"""
     
     def __init__(self, ent_tot, rel_tot, dim = 100):
