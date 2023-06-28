36181bfc2552fdc0327f694622454e79b464fc38
LuYF-Lemon-love <3555028709@qq.com>
2023-06-28 20:04:30 +0800

    修复SimplE代码

--- pybind11_ke_examples/train_distmult_WN18RR_adv.py
+++ pybind11_ke_examples/train_distmult_WN18RR_adv.py
@@ -1,3 +1,28 @@
+"""
+`RESCAL-FB15K237 <train_rescal_FB15K237.html>`_ ||
+`TransE-FB15K237 <train_transe_FB15K237.html>`_ ||
+`TransH-FB15K237 <train_transh_FB15K237.html>`_ ||
+**DistMult-WN18RR** ||
+`TransD-FB15K237 <train_transd_FB15K237.html>`_ ||
+`HolE-WN18RR <train_hole_WN18RR.html>`_ ||
+`ComplEx-WN18RR <train_complex_WN18RR.html>`_ ||
+`Analogy-WN18RR <train_analogy_WN18RR.html>`_ ||
+`SimplE-WN18RR <train_simple_WN18RR.html>`_ ||
+`RotatE-WN18RR <train_rotate_WN18RR_adv.html>`_
+
+DistMult-WN18RR-adv
+=====================
+这一部分介绍如何用在 WN18RR 知识图谱上训练 DistMult，应用 RotatE 提出的自我对抗负采样损失函数进行模型训练。
+
+DistMult 原论文: `Embedding Entities and Relations for Learning and Inference in Knowledge Bases <https://arxiv.org/abs/1412.6575>`__ 。
+
+导入数据
+-----------------
+pybind11-OpenKE 有两个工具用于导入数据: :py:class:`pybind11_ke.data.TrainDataLoader` 和
+:py:class:`pybind11_ke.data.TestDataLoader`。
+
+"""
+
 import openke
 from openke.config import Trainer, Tester
 from openke.module.model import DistMult
@@ -5,9 +30,15 @@ from openke.module.loss import SigmoidLoss
 from openke.module.strategy import NegativeSampling
 from openke.data import TrainDataLoader, TestDataLoader
 
+######################################################################
+# pybind11-KE 提供了很多数据集，它们很多都是 KGE 原论文发表时附带的数据集。
+# 
+# :py:class:`pybind11_ke.data.TrainDataLoader` 和 :py:class:`pybind11_ke.data.TestDataLoader`
+# 都包含 ``in_path`` 用于传递数据集目录。
+
 # dataloader for training
 train_dataloader = TrainDataLoader(
-	in_path = "./benchmarks/WN18RR/", 
+	in_path = "../benchmarks/WN18RR/", 
 	batch_size = 2000,
 	threads = 8,
 	sampling_mode = "cross", 
@@ -18,7 +49,17 @@ train_dataloader = TrainDataLoader(
 )
 
 # dataloader for test
-test_dataloader = TestDataLoader("./benchmarks/WN18RR/", "link")
+test_dataloader = TestDataLoader("../benchmarks/WN18RR/", "link")
+
+######################################################################
+# --------------
+#
+
+################################
+# 导入模型
+# ------------------
+# pybind11-OpenKE 提供了很多 KGE 模型，它们都是目前最常用的基线模型。我们下面将要导入
+# :py:class:`pybind11_ke.module.model.DistMult`，它是最简单的双线性模型。
 
 # define the model
 distmult = DistMult(
@@ -29,6 +70,19 @@ distmult = DistMult(
 	epsilon = 2.0
 )
 
+######################################################################
+# --------------
+#
+
+
+#####################################################################
+# 损失函数
+# ----------------------------------------
+# 我们这里使用了逻辑损失函数：:py:class:`pybind11_ke.module.loss.SigmoidLoss`，
+# :py:class:`pybind11_ke.module.strategy.NegativeSampling` 对
+# :py:class:`pybind11_ke.module.loss.SigmoidLoss` 进行了封装，加入权重衰减等额外项。
+# 除此之外，我们使用 ``adv_temperature`` 开启了 RotatE 提出的自我对抗负采样。
+
 # define the loss function
 model = NegativeSampling(
 	model = distmult, 
@@ -37,12 +91,33 @@ model = NegativeSampling(
 	l3_regul_rate = 0.000005
 )
 
+######################################################################
+# --------------
+#
+
+######################################################################
+# 训练模型
+# -------------
+# pybind11-OpenKE 将训练循环包装成了 :py:class:`pybind11_ke.config.Trainer`，
+# 可以运行它的 :py:meth:`pybind11_ke.config.Trainer.run` 函数进行模型学习。
+
 # train the model
-trainer = Trainer(model = model, data_loader = train_dataloader, train_times = 400, alpha = 0.002, use_gpu = True, opt_method = "adam")
+trainer = Trainer(model = model, data_loader = train_dataloader,
+                  train_times = 400, alpha = 0.002, use_gpu = True, opt_method = "adam")
 trainer.run()
-distmult.save_checkpoint('./checkpoint/distmult.ckpt')
+distmult.save_checkpoint('../checkpoint/distmult.ckpt')
+
+######################################################################
+# --------------
+#
+
+######################################################################
+# 评估模型
+# -------------
+# 与模型训练一样，pybind11-OpenKE 将评估模型包装成了 :py:class:`pybind11_ke.config.Tester`，
+# 可以运行它的 :py:meth:`pybind11_ke.config.Tester.run_link_prediction` 函数进行链接预测。
 
 # test the model
-distmult.load_checkpoint('./checkpoint/distmult.ckpt')
+distmult.load_checkpoint('../checkpoint/distmult.ckpt')
 tester = Tester(model = distmult, data_loader = test_dataloader, use_gpu = True)
 tester.run_link_prediction(type_constrain = False)
