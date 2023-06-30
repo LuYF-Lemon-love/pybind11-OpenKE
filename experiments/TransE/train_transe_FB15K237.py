a205ad8c7abe15b105a1cdb9043e3c9458ae9844
LuYF-Lemon-love <3555028709@qq.com>
2023-06-30 14:40:12 +0800

    开始 pybind11-OpenKE 的独立开发

--- experiments/TransE/train_transe_FB15K237.py
+++ experiments/TransE/train_transe_FB15K237.py
@@ -1,28 +1,28 @@
 """
-`RESCAL-FB15K237 <train_rescal_FB15K237.html>`_ ||
-**TransE-FB15K237** ||
+**TransE-FB15K** ||
 `TransE-WN18RR-adv <train_transe_WN18_adv_sigmoidloss.html>`_ ||
 `TransH-FB15K237 <train_transh_FB15K237.html>`_ ||
-`DistMult-WN18RR <train_distmult_WN18RR.html>`_ ||
-`DistMult-WN18RR-adv <train_distmult_WN18RR_adv.html>`_ ||
-`TransD-FB15K237 <train_transd_FB15K237.html>`_ ||
-`HolE-WN18RR <train_hole_WN18RR.html>`_ ||
-`ComplEx-WN18RR <train_complex_WN18RR.html>`_ ||
-`Analogy-WN18RR <train_analogy_WN18RR.html>`_ ||
 `SimplE-WN18RR <train_simple_WN18RR.html>`_ ||
 `RotatE-WN18RR <train_rotate_WN18RR_adv.html>`_
 
 TransE-FB15K
 ===================
-这一部分介绍如何用在 FB15K237 知识图谱上训练 TransE。
+这一部分介绍如何用在 FB15k 知识图谱上训练 TransE。
 
 TransE 原论文: `Translating Embeddings for Modeling Multi-relational Data <https://proceedings.neurips.cc/paper_files/paper/2013/hash/1cecc7a77928ca8133fa24680a88d2f9-Abstract.html>`__。
+
+下面是 TransE 原论文给出的 FB15k 的模型超参数，使用 SGD 进行训练的。
+
+============= =========== ========== ============ ===========
+   向量维度       学习率     margin      距离函数     epochs
+============= =========== ========== ============ ===========
+     50           0.01        1           L1         1, 000
+============= =========== ========== ============ ===========
 
 导入数据
 -----------------
 pybind11-OpenKE 有两个工具用于导入数据: :py:class:`pybind11_ke.data.TrainDataLoader` 和
 :py:class:`pybind11_ke.data.TestDataLoader`。
-
 """
 
 from pybind11_ke.config import Trainer, Tester
@@ -39,7 +39,7 @@ from pybind11_ke.data import TrainDataLoader, TestDataLoader
 
 # dataloader for training
 train_dataloader = TrainDataLoader(
-	in_path = "../benchmarks/FB15K237/", 
+	in_path = "../benchmarks/FB15K/", 
 	nbatches = 100,
 	threads = 8, 
 	sampling_mode = "normal", 
@@ -49,7 +49,7 @@ train_dataloader = TrainDataLoader(
 	neg_rel = 0)
 
 # dataloader for test
-test_dataloader = TestDataLoader("../benchmarks/FB15K237/", "link")
+test_dataloader = TestDataLoader("../benchmarks/FB15K/", "link")
 
 ######################################################################
 # --------------
@@ -65,7 +65,7 @@ test_dataloader = TestDataLoader("../benchmarks/FB15K237/", "link")
 transe = TransE(
 	ent_tot = train_dataloader.get_ent_tot(),
 	rel_tot = train_dataloader.get_rel_tot(),
-	dim = 200, 
+	dim = 50, 
 	p_norm = 1, 
 	norm_flag = True)
 
@@ -84,7 +84,7 @@ transe = TransE(
 # define the loss function
 model = NegativeSampling(
 	model = transe, 
-	loss = MarginLoss(margin = 5.0),
+	loss = MarginLoss(margin = 1.0),
 	batch_size = train_dataloader.get_batch_size()
 )
 
@@ -100,7 +100,7 @@ model = NegativeSampling(
 
 # train the model
 trainer = Trainer(model = model, data_loader = train_dataloader,
-                  train_times = 1000, alpha = 1.0, use_gpu = True)
+                  train_times = 1000, alpha = 0.01, use_gpu = True)
 trainer.run()
 transe.save_checkpoint('../checkpoint/transe.ckpt')
 
