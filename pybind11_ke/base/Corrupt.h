a35fffcc37e7a7bc63f735999575db8c30d4e45b
LuYF-Lemon-love <3555028709@qq.com>
2023-07-04 15:44:45 +0800

    精简代码

--- pybind11_ke/base/Corrupt.h
+++ pybind11_ke/base/Corrupt.h
@@ -16,26 +16,26 @@
 INT corrupt_head(INT id, INT h, INT r) {
 	INT lef, rig, mid, ll, rr;
 
-	// lef: head(h) 在 trainHead 中第一次出现的前一个位置
-	// rig: head(h) 在 trainHead 中最后一次出现的位置
-	lef = lefHead[h] - 1;
-	rig = rigHead[h];
+	// lef: head(h) 在 train_head 中第一次出现的前一个位置
+	// rig: head(h) 在 train_head 中最后一次出现的位置
+	lef = left_head[h] - 1;
+	rig = right_head[h];
 	while (lef + 1 < rig) {
 		mid = (lef + rig) >> 1;
 		// 二分查找算法变体
 		// 由于 >= -> rig，所以 rig 最终在第一个 r 的位置
-		if (trainHead[mid].r >= r) rig = mid; else
+		if (train_head[mid].r >= r) rig = mid; else
 		lef = mid;
 	}
 	ll = rig;
 
-	lef = lefHead[h];
-	rig = rigHead[h] + 1;
+	lef = left_head[h];
+	rig = right_head[h] + 1;
 	while (lef + 1 < rig) {
 		mid = (lef + rig) >> 1;
 		// 二分查找算法变体
 		// 由于 <= -> lef，所以 lef 最终在最后一个 r 的位置
-		if (trainHead[mid].r <= r) lef = mid; else
+		if (train_head[mid].r <= r) lef = mid; else
 		rig = mid;
 	}
 	rr = lef;
@@ -44,18 +44,18 @@ INT corrupt_head(INT id, INT h, INT r) {
 	INT tmp = rand_max(id, entity_total - (rr - ll + 1));
 
 	// 第一种：tmp 小于第一个 r 对应的 tail
-	if (tmp < trainHead[ll].t) return tmp;
+	if (tmp < train_head[ll].t) return tmp;
 
 	// 第二种：tmp 大于最后一个 r 对应的 tail
-	if (tmp > trainHead[rr].t - rr + ll - 1) return tmp + rr - ll + 1;
+	if (tmp > train_head[rr].t - rr + ll - 1) return tmp + rr - ll + 1;
 
 	// 第三种：由于 (>= -> rig), (lef + 1 < rig), (tmp + lef - ll + 1)
 	// 因此最终返回取值为 (train_head[lef].t, train_head[rig].t) 的 tail
 	lef = ll, rig = rr + 1;
 	while (lef + 1 < rig) {
 		mid = (lef + rig) >> 1;
-		// 类似 tmp > trainHead[rr].t - rr + ll - 1
-		if (trainHead[mid].t - mid + ll - 1 < tmp)
+		// 类似 tmp > train_head[rr].t - rr + ll - 1
+		if (train_head[mid].t - mid + ll - 1 < tmp)
 			lef = mid;
 		else 
 			rig = mid;
@@ -69,26 +69,26 @@ INT corrupt_head(INT id, INT h, INT r) {
 INT corrupt_tail(INT id, INT t, INT r) {
 	INT lef, rig, mid, ll, rr;
 
-	// lef: tail(t) 在 trainTail 中第一次出现的前一个位置
-	// rig: tail(t) 在 trainTail 中最后一次出现的位置
-	lef = lefTail[t] - 1;
-	rig = rigTail[t];
+	// lef: tail(t) 在 train_tail 中第一次出现的前一个位置
+	// rig: tail(t) 在 train_tail 中最后一次出现的位置
+	lef = left_tail[t] - 1;
+	rig = right_tail[t];
 	while (lef + 1 < rig) {
 		mid = (lef + rig) >> 1;
 		// 二分查找算法变体
 		// 由于 >= -> rig，所以 rig 最终在第一个 r 的位置
-		if (trainTail[mid].r >= r) rig = mid; else
+		if (train_tail[mid].r >= r) rig = mid; else
 		lef = mid;
 	}
 	ll = rig;
 
-	lef = lefTail[t];
-	rig = rigTail[t] + 1;
+	lef = left_tail[t];
+	rig = right_tail[t] + 1;
 	while (lef + 1 < rig) {
 		mid = (lef + rig) >> 1;
 		// 二分查找算法变体
 		// 由于 <= -> lef，所以 lef 最终在最后一个 r 的位置
-		if (trainTail[mid].r <= r) lef = mid; else
+		if (train_tail[mid].r <= r) lef = mid; else
 		rig = mid;
 	}
 	rr = lef;
@@ -97,18 +97,18 @@ INT corrupt_tail(INT id, INT t, INT r) {
 	INT tmp = rand_max(id, entity_total - (rr - ll + 1));
 
 	// 第一种：tmp 小于第一个 r 对应的 head
-	if (tmp < trainTail[ll].h) return tmp;
+	if (tmp < train_tail[ll].h) return tmp;
 
 	// 第二种：tmp 大于最后一个 r 对应的 head
-	if (tmp > trainTail[rr].h - rr + ll - 1) return tmp + rr - ll + 1;
+	if (tmp > train_tail[rr].h - rr + ll - 1) return tmp + rr - ll + 1;
 
 	// 第三种：由于 (>= -> rig), (lef + 1 < rig), (tmp + lef - ll + 1)
 	// 因此最终返回取值为 (train_tail[lef].h, train_tail[rig].h) 的 head
 	lef = ll, rig = rr + 1;
 	while (lef + 1 < rig) {
 		mid = (lef + rig) >> 1;
-		// 类似 tmp > trainTail[rr].h - rr + ll - 1
-		if (trainTail[mid].h - mid + ll - 1 < tmp)
+		// 类似 tmp > train_tail[rr].h - rr + ll - 1
+		if (train_tail[mid].h - mid + ll - 1 < tmp)
 			lef = mid;
 		else 
 			rig = mid;
@@ -122,26 +122,26 @@ INT corrupt_tail(INT id, INT t, INT r) {
 INT corrupt_rel(INT id, INT h, INT t, INT r, bool p = false) {
 	INT lef, rig, mid, ll, rr;
 
-	// lef: head(h) 在 trainRel 中第一次出现的前一个位置
-	// rig: head(h) 在 trainRel 中最后一次出现的位置
-	lef = lefRel[h] - 1;
-	rig = rigRel[h];
+	// lef: head(h) 在 train_rel 中第一次出现的前一个位置
+	// rig: head(h) 在 train_rel 中最后一次出现的位置
+	lef = left_rel[h] - 1;
+	rig = right_rel[h];
 	while (lef + 1 < rig) {
 		mid = (lef + rig) >> 1;
 		// 二分查找算法变体
 		// 由于 >= -> rig，所以 rig 最终在第一个 t 的位置
-		if (trainRel[mid].t >= t) rig = mid; else
+		if (train_rel[mid].t >= t) rig = mid; else
 		lef = mid;
 	}
 	ll = rig;
 
-	lef = lefRel[h];
-	rig = rigRel[h] + 1;
+	lef = left_rel[h];
+	rig = right_rel[h] + 1;
 	while (lef + 1 < rig) {
 		mid = (lef + rig) >> 1;
 		// 二分查找算法变体
 		// 由于 <= -> lef，所以 lef 最终在最后一个 t 的位置
-		if (trainRel[mid].t <= t) lef = mid; else
+		if (train_rel[mid].t <= t) lef = mid; else
 		rig = mid;
 	}
 	rr = lef;
@@ -157,13 +157,13 @@ INT corrupt_rel(INT id, INT h, INT t, INT r, bool p = false) {
 		REAL sum = 1;
 		bool *record = (bool *)calloc(relation_total - 1, sizeof(bool));
 		for (INT i = ll; i <= rr; ++i){
-			if (trainRel[i].r > r){
-				sum -= prob[start + trainRel[i].r-1];
-				record[trainRel[i].r-1] = true;
+			if (train_rel[i].r > r){
+				sum -= prob[start + train_rel[i].r-1];
+				record[train_rel[i].r-1] = true;
 			}
-			else if (trainRel[i].r < r){
-				sum -= prob[start + trainRel[i].r];
-				record[trainRel[i].r] = true;
+			else if (train_rel[i].r < r){
+				sum -= prob[start + train_rel[i].r];
+				record[train_rel[i].r] = true;
 			}
 		}		
 		REAL *prob_tmp = (REAL *)calloc(relation_total-(rr-ll+1), sizeof(REAL));
@@ -191,18 +191,18 @@ INT corrupt_rel(INT id, INT h, INT t, INT r, bool p = false) {
 	}
 
 	// 第一种：tmp 小于第一个 t 对应的 relation
-	if (tmp < trainRel[ll].r) return tmp;
+	if (tmp < train_rel[ll].r) return tmp;
 
 	// 第二种：tmp 大于最后一个 t 对应的 relation
-	if (tmp > trainRel[rr].r - rr + ll - 1) return tmp + rr - ll + 1;
+	if (tmp > train_rel[rr].r - rr + ll - 1) return tmp + rr - ll + 1;
 	
 	// 第三种：由于 (>= -> rig), (lef + 1 < rig), (tmp + lef - ll + 1)
 	// 因此最终返回取值为 (train_head[lef].t, train_head[rig].t) 的 tail
 	lef = ll, rig = rr + 1;
 	while (lef + 1 < rig) {
 		mid = (lef + rig) >> 1;
-		// 类似 tmp > trainRel[rr].r - rr + ll - 1
-		if (trainRel[mid].r - mid + ll - 1 < tmp)
+		// 类似 tmp > train_rel[rr].r - rr + ll - 1
+		if (train_rel[mid].r - mid + ll - 1 < tmp)
 			lef = mid;
 		else 
 			rig = mid;
--- pybind11_ke/base/Reader.h
+++ pybind11_ke/base/Reader.h
@@ -14,17 +14,17 @@
 #include <iostream>
 #include <cmath>
 
-INT *freqRel, *freqEnt;
-INT *lefHead, *rigHead;
-INT *lefTail, *rigTail;
-INT *lefRel, *rigRel;
+INT *freq_rel, *freq_ent;
+INT *left_head, *right_head;
+INT *left_tail, *right_tail;
+INT *left_rel, *right_rel;
 REAL *left_mean, *right_mean;
 REAL *prob;
 
 Triple *train_list;
-Triple *trainHead;
-Triple *trainTail;
-Triple *trainRel;
+Triple *train_head;
+Triple *train_tail;
+Triple *train_rel;
 
 INT *testLef, *testRig;
 INT *validLef, *validRig;
@@ -69,133 +69,133 @@ void importTrainFiles() {
     // rel_file: 定义于 Setting.h
     fin = fopen(rel_file.c_str(), "r");
     // relation2id.txt 第一行是关系的个数
-	tmp = fscanf(fin, "%ld", &relation_total);
+    tmp = fscanf(fin, "%ld", &relation_total);
     std::cout << "The total of relations is " << relation_total
-            << "." << std::endl;
-	fclose(fin);
+        << "." << std::endl;
+    fclose(fin);
 
     // 读取实体的个数
     // ent_file: defined in Setting.h
     fin = fopen(ent_file.c_str(), "r");
     // entity2id.txt 第一行是实体的个数
-	tmp = fscanf(fin, "%ld", &entity_total);
+    tmp = fscanf(fin, "%ld", &entity_total);
     std::cout << "The total of entities is " << entity_total
-            << "." << std::endl;
-	fclose(fin);
+        << "." << std::endl;
+    fclose(fin);
 
     // 读取训练数据集
     // train_file: 定义于 Setting.h
     fin = fopen(train_file.c_str(), "r");
     // train2id.txt 第一行是三元组的个数.
-	tmp = fscanf(fin, "%ld", &train_total);
+    tmp = fscanf(fin, "%ld", &train_total);
     // train_list: 保存训练集中的三元组集合.
-	train_list = (Triple *)calloc(train_total, sizeof(Triple));
-	trainHead = (Triple *)calloc(train_total, sizeof(Triple));
-	trainTail = (Triple *)calloc(train_total, sizeof(Triple));
-	trainRel = (Triple *)calloc(train_total, sizeof(Triple));
-    // freqRel, freqEnt: 元素值被初始化为 0.
-	freqRel = (INT *)calloc(relation_total, sizeof(INT));
-	freqEnt = (INT *)calloc(entity_total, sizeof(INT));
+    train_list = (Triple *)calloc(train_total, sizeof(Triple));
+    train_head = (Triple *)calloc(train_total, sizeof(Triple));
+    train_tail = (Triple *)calloc(train_total, sizeof(Triple));
+    train_rel = (Triple *)calloc(train_total, sizeof(Triple));
+    // freq_rel, freq_ent: 元素值被初始化为 0.
+    freq_rel = (INT *)calloc(relation_total, sizeof(INT));
+    freq_ent = (INT *)calloc(entity_total, sizeof(INT));
     // 读取训练集三元组集合, 保存在 train_list.
-	for (INT i = 0; i < train_total; i++) {
-		tmp = fscanf(fin, "%ld", &train_list[i].h);
-		tmp = fscanf(fin, "%ld", &train_list[i].t);
-		tmp = fscanf(fin, "%ld", &train_list[i].r);
-	}
-	fclose(fin);
+    for (INT i = 0; i < train_total; i++) {
+        tmp = fscanf(fin, "%ld", &train_list[i].h);
+        tmp = fscanf(fin, "%ld", &train_list[i].t);
+        tmp = fscanf(fin, "%ld", &train_list[i].r);
+    }
+    fclose(fin);
     // 对 train_list 中的三元组排序 (比较顺序: h, r, t).
-	std::sort(train_list, train_list + train_total, Triple::cmp_head);
+    std::sort(train_list, train_list + train_total, Triple::cmp_head);
     // tmp: 保存训练集三元组的个数
-	tmp = train_total; train_total = 1;
-	trainHead[0] = trainTail[0] = trainRel[0] = train_list[0];
-    // freqEnt: 保存实体在训练集中出现的总数
-    // freqRel: 保存关系在训练集中出现的总数
-	freqEnt[train_list[0].t] += 1;
-	freqEnt[train_list[0].h] += 1;
-	freqRel[train_list[0].r] += 1;
+    tmp = train_total; train_total = 1;
+    train_head[0] = train_tail[0] = train_rel[0] = train_list[0];
+    // freq_ent: 保存实体在训练集中出现的总数
+    // freq_rel: 保存关系在训练集中出现的总数
+    freq_ent[train_list[0].t] += 1;
+    freq_ent[train_list[0].h] += 1;
+    freq_rel[train_list[0].r] += 1;
     // 对训练集中的三元组去重
-	for (INT i = 1; i < tmp; i++)
-		if (train_list[i].h != train_list[i - 1].h 
-        || train_list[i].r != train_list[i - 1].r 
-        || train_list[i].t != train_list[i - 1].t) {
-			trainHead[train_total] = trainTail[train_total] 
-                = trainRel[train_total] = train_list[train_total] 
+    for (INT i = 1; i < tmp; i++)
+        if (train_list[i].h != train_list[i - 1].h
+            || train_list[i].r != train_list[i - 1].r
+            || train_list[i].t != train_list[i - 1].t) {
+            train_head[train_total] = train_tail[train_total]
+                = train_rel[train_total] = train_list[train_total]
                 = train_list[i];
-			train_total++;
-			freqEnt[train_list[i].t]++;
-			freqEnt[train_list[i].h]++;
-			freqRel[train_list[i].r]++;
-		}
+            train_total++;
+            freq_ent[train_list[i].t]++;
+            freq_ent[train_list[i].h]++;
+            freq_rel[train_list[i].r]++;
+        }
 
-    // trainHead: 以 h, r, t 排序
-    // trainTail: 以 t, r, h 排序
-    // trainRel: 以 h, t, r 排序
-	std::sort(trainHead, trainHead + train_total, Triple::cmp_head);
-	std::sort(trainTail, trainTail + train_total, Triple::cmp_tail);
-	std::sort(trainRel, trainRel + train_total, Triple::cmp_rel);
+    // train_head: 以 h, r, t 排序
+    // train_tail: 以 t, r, h 排序
+    // train_rel: 以 h, t, r 排序
+    std::sort(train_head, train_head + train_total, Triple::cmp_head);
+    std::sort(train_tail, train_tail + train_total, Triple::cmp_tail);
+    std::sort(train_rel, train_rel + train_total, Triple::cmp_rel);
     std::cout << "The total of train triples is " << train_total
-            << "." << std::endl;
-
-	lefHead = (INT *)calloc(entity_total, sizeof(INT));
-	rigHead = (INT *)calloc(entity_total, sizeof(INT));
-	lefTail = (INT *)calloc(entity_total, sizeof(INT));
-	rigTail = (INT *)calloc(entity_total, sizeof(INT));
-	lefRel = (INT *)calloc(entity_total, sizeof(INT));
-	rigRel = (INT *)calloc(entity_total, sizeof(INT));
-    // rigHead, rigTail, rigRel 初始化为 -1
-	memset(rigHead, -1, sizeof(INT)*entity_total);
-	memset(rigTail, -1, sizeof(INT)*entity_total);
-	memset(rigRel, -1, sizeof(INT)*entity_total);
-	for (INT i = 1; i < train_total; i++) {
-        // lefHead (entity_total): 存储每种实体 (head) 在 trainHead 中第一次出现的位置
-        // rigHead (entity_total): 存储每种实体 (head) 在 trainHead 中最后一次出现的位置
-		if (trainHead[i].h != trainHead[i - 1].h) {
-			rigHead[trainHead[i - 1].h] = i - 1;
-			lefHead[trainHead[i].h] = i;
-		}
-        // lefTail (entity_total): 存储每种实体 (tail) 在 trainTail 中第一次出现的位置
-        // rigTail (entity_total): 存储每种实体 (tail) 在 trainTail 中最后一次出现的位置
-		if (trainTail[i].t != trainTail[i - 1].t) {
-			rigTail[trainTail[i - 1].t] = i - 1;
-			lefTail[trainTail[i].t] = i;
-		}
-        // lefRel (entity_total): 存储每种实体 (head) 在 trainRel 中第一次出现的位置
-        // rigRel (entity_total): 存储每种实体 (head) 在 trainRel 中最后一次出现的位置
-		if (trainRel[i].h != trainRel[i - 1].h) {
-			rigRel[trainRel[i - 1].h] = i - 1;
-			lefRel[trainRel[i].h] = i;
-		}
-	}
-	lefHead[trainHead[0].h] = 0;
-	rigHead[trainHead[train_total - 1].h] = train_total - 1;
-	lefTail[trainTail[0].t] = 0;
-	rigTail[trainTail[train_total - 1].t] = train_total - 1;
-	lefRel[trainRel[0].h] = 0;
-	rigRel[trainRel[train_total - 1].h] = train_total - 1;
-
-	// 获得 left_mean、right_mean，为 train_mode 中的 bern_flag 做准备
-	// 在训练过程中，我们能够构建负三元组进行负采样
-	// bern 算法能根据特定关系的 head 和 tail 种类的比值，选择构建适当的负三元组
-	// train_mode 中的 bern_flag: pr = left_mean / (left_mean + right_mean)
-	// 因此为训练而构建的负三元组比 = tail / (tail + head)
-	left_mean = (REAL *)calloc(relation_total,sizeof(REAL));
-	right_mean = (REAL *)calloc(relation_total,sizeof(REAL));
-	for (INT i = 0; i < entity_total; i++) {
-		for (INT j = lefHead[i] + 1; j <= rigHead[i]; j++)
-			if (trainHead[j].r != trainHead[j - 1].r)
-				left_mean[trainHead[j].r] += 1.0;
-		if (lefHead[i] <= rigHead[i])
-			left_mean[trainHead[lefHead[i]].r] += 1.0;
-		for (INT j = lefTail[i] + 1; j <= rigTail[i]; j++)
-			if (trainTail[j].r != trainTail[j - 1].r)
-				right_mean[trainTail[j].r] += 1.0;
-		if (lefTail[i] <= rigTail[i])
-			right_mean[trainTail[lefTail[i]].r] += 1.0;
-	}
-	for (INT i = 0; i < relation_total; i++) {
-		left_mean[i] = freqRel[i] / left_mean[i];
-		right_mean[i] = freqRel[i] / right_mean[i];
-	}
+        << "." << std::endl;
+    
+    left_head = (INT *)calloc(entity_total, sizeof(INT));
+    right_head = (INT *)calloc(entity_total, sizeof(INT));
+    left_tail = (INT *)calloc(entity_total, sizeof(INT));
+    right_tail = (INT *)calloc(entity_total, sizeof(INT));
+	left_rel = (INT *)calloc(entity_total, sizeof(INT));
+	right_rel = (INT *)calloc(entity_total, sizeof(INT));
+    // right_head, right_tail, right_rel 初始化为 -1
+	memset(right_head, -1, sizeof(INT)*entity_total);
+	memset(right_tail, -1, sizeof(INT)*entity_total);
+	memset(right_rel, -1, sizeof(INT)*entity_total);
+    for (INT i = 1; i < train_total; i++) {
+        // left_head (entity_total): 存储每种实体 (head) 在 train_head 中第一次出现的位置
+        // right_head (entity_total): 存储每种实体 (head) 在 train_head 中最后一次出现的位置
+        if (train_head[i].h != train_head[i - 1].h) {
+            right_head[train_head[i - 1].h] = i - 1;
+            left_head[train_head[i].h] = i;
+        }
+        // left_tail (entity_total): 存储每种实体 (tail) 在 train_tail 中第一次出现的位置
+        // right_tail (entity_total): 存储每种实体 (tail) 在 train_tail 中最后一次出现的位置
+        if (train_tail[i].t != train_tail[i - 1].t) {
+            right_tail[train_tail[i - 1].t] = i - 1;
+            left_tail[train_tail[i].t] = i;
+        }
+        // left_rel (entity_total): 存储每种实体 (head) 在 train_rel 中第一次出现的位置
+        // right_rel (entity_total): 存储每种实体 (head) 在 train_rel 中最后一次出现的位置
+        if (train_rel[i].h != train_rel[i - 1].h) {
+            right_rel[train_rel[i - 1].h] = i - 1;
+            left_rel[train_rel[i].h] = i;
+        }
+    }
+    left_head[train_head[0].h] = 0;
+    right_head[train_head[train_total - 1].h] = train_total - 1;
+    left_tail[train_tail[0].t] = 0;
+    right_tail[train_tail[train_total - 1].t] = train_total - 1;
+    left_rel[train_rel[0].h] = 0;
+    right_rel[train_rel[train_total - 1].h] = train_total - 1;
+    
+    // 获得 left_mean、right_mean，为 train_mode 中的 bern_flag 做准备
+    // 在训练过程中，我们能够构建负三元组进行负采样
+    // bern 算法能根据特定关系的 head 和 tail 种类的比值，选择构建适当的负三元组
+    // train_mode 中的 bern_flag: pr = left_mean / (left_mean + right_mean)
+    // 因此为训练而构建的负三元组比 = tail / (tail + head)
+    left_mean = (REAL *)calloc(relation_total, sizeof(REAL));
+    right_mean = (REAL *)calloc(relation_total, sizeof(REAL));
+    for (INT i = 0; i < entity_total; i++) {
+        for (INT j = left_head[i] + 1; j <= right_head[i]; j++)
+            if (train_head[j].r != train_head[j - 1].r)
+                left_mean[train_head[j].r] += 1.0;
+        if (left_head[i] <= right_head[i])
+            left_mean[train_head[left_head[i]].r] += 1.0;
+        for (INT j = left_tail[i] + 1; j <= right_tail[i]; j++)
+            if (train_tail[j].r != train_tail[j - 1].r)
+                right_mean[train_tail[j].r] += 1.0;
+        if (left_tail[i] <= right_tail[i])
+            right_mean[train_tail[left_tail[i]].r] += 1.0;
+    }
+    for (INT i = 0; i < relation_total; i++) {
+        left_mean[i] = freq_rel[i] / left_mean[i];
+        right_mean[i] = freq_rel[i] / right_mean[i];
+    }
 }
 
 Triple *testList;
