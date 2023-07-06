3497fa5d8d0eff0a2709c35a82b6554c0ded4723
LuYF-Lemon-love <3555028709@qq.com>
2023-07-06 20:16:10 +0800

    精简代码

--- pybind11_ke/config/TrainerDataParallel.py
+++ pybind11_ke/config/TrainerDataParallel.py
@@ -27,7 +27,6 @@ import torch
 import torch.optim as optim
 import os
 import torch.multiprocessing as mp
-from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
 from ..utils.Timer import Timer
