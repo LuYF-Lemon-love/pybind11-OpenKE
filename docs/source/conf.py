61f839d707dd27908c96cf187794dbf9723ad6d9
LuYF-Lemon-love <3555028709@qq.com>
2023-12-22 20:30:16 +0800

    重建文档

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = "Pybind11-OpenKE"
+copyright = "2023, LuYF-Lemon-love"
+author = "LuYF-Lemon-love"
 
 release = '0.1'
 version = '0.1.0'
