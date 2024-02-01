26ae1c42d2b3047f3e63cdc9fac90be9b4f3fb70
LuYF-Lemon-love <3555028709@qq.com>
2024-02-01 21:12:35 +0800

    修复 DistMult 实现

--- pybind11_ke/module/model/DistMult.py
+++ pybind11_ke/module/model/DistMult.py
@@ -32,36 +32,78 @@ class DistMult(Model):
 
 	例子::
 
-		from pybind11_ke.config import Trainer, Tester
+		from pybind11_ke.utils import WandbLogger
+		from pybind11_ke.data import KGEDataLoader, BernSampler, TradTestSampler
 		from pybind11_ke.module.model import DistMult
 		from pybind11_ke.module.loss import SoftplusLoss
 		from pybind11_ke.module.strategy import NegativeSampling
+		from pybind11_ke.config import Trainer, Tester
+		
+		wandb_logger = WandbLogger(
+			project="pybind11-ke",
+			name="DistMult-WN18RR",
+			config=dict(
+				in_path = '../../benchmarks/WN18RR/',
+				batch_size = 4096,
+				neg_ent = 25,
+				test = True,
+				test_batch_size = 10,
+				num_workers = 16,
+				dim = 200,
+				regul_rate = 1.0,
+				use_gpu = True,
+				device = 'cuda:0',
+				epochs = 2000,
+				lr = 0.5,
+				opt_method = 'adagrad',
+				valid_interval = 100,
+				log_interval = 100,
+				save_interval = 100,
+				save_path = '../../checkpoint/distMult.pth'
+			)
+		)
+		
+		config = wandb_logger.config
+		
+		# dataloader for training
+		dataloader = KGEDataLoader(
+			in_path = config.in_path, 
+			batch_size = config.batch_size,
+			neg_ent = config.neg_ent,
+			test = config.test,
+			test_batch_size = config.test_batch_size,
+			num_workers = config.num_workers,
+			train_sampler = BernSampler,
+			test_sampler = TradTestSampler
+		)
 		
 		# define the model
 		distmult = DistMult(
-			ent_tol = train_dataloader.get_ent_tol(),
-			rel_tol = train_dataloader.get_rel_tol(),
+			ent_tol = dataloader.get_ent_tol(),
+			rel_tol = dataloader.get_rel_tol(),
 			dim = config.dim
 		)
 		
 		# define the loss function
 		model = NegativeSampling(
 			model = distmult, 
-			loss = SoftplusLoss(),
-			batch_size = train_dataloader.get_batch_size(), 
+			loss = SoftplusLoss(), 
 			regul_rate = config.regul_rate
 		)
 		
 		# test the model
-		tester = Tester(model = distmult, data_loader = test_dataloader, use_gpu = config.use_gpu, device = config.device)
+		tester = Tester(model = distmult, data_loader = dataloader, use_gpu = config.use_gpu, device = config.device)
 		
 		# train the model
-		trainer = Trainer(model = model, data_loader = train_dataloader, epochs = config.epochs,
+		trainer = Trainer(model = model, data_loader = dataloader.train_dataloader(), epochs = config.epochs,
 			lr = config.lr, opt_method = config.opt_method, use_gpu = config.use_gpu, device = config.device,
 			tester = tester, test = config.test, valid_interval = config.valid_interval,
 			log_interval = config.log_interval, save_interval = config.save_interval,
 			save_path = config.save_path, use_wandb = True)
 		trainer.run()
+		
+		# close your wandb run
+		wandb_logger.finish()
 	"""
 
 	def __init__(
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m "修复 rescal 实现"
+git commit -m "修复 DistMult 实现"
 git push
