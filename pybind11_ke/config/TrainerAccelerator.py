69e01616da0be12e462efde3ecdab72a048a7912
LuYF-Lemon-love <3555028709@qq.com>
2024-04-27 22:42:27 +0800

    修改 pybind11_ke/config/TrainerAccelerator.py

--- pybind11_ke/config/TrainerAccelerator.py
+++ pybind11_ke/config/TrainerAccelerator.py
@@ -18,10 +18,17 @@ def accelerator_prepare(*args: List[Any]) -> List[Any]:
 
 	"""
 	由于分布式并行依赖于 `accelerate <https://github.com/huggingface/accelerate>`_ ，因此，需要利用 Accelerator 为分布式训练准备对象。
+	
+	例子::
+
+		dataloader, model, accelerator = accelerator_prepare(
+		    dataloader,
+		    model
+		)
 
 	:param args: :py:class:`pybind11_ke.data.KGEDataLoader` 和 :py:class:`pybind11_ke.module.strategy.Strategy` 。
 	:type args: typing.List[typing.Any]
-	:returns: 包装好的对象。
+	:returns: 包装好的对象列表和 Accelerator() 对象。
 	:rtype: typing.List[typing.Any]
 	"""
 	
