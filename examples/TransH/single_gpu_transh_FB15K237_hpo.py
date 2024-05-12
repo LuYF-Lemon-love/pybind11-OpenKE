fa591a51b22d904aaec1f488012c9dad6d353b6f
LuYF-Lemon-love <3555028709@qq.com>
2024-05-12 15:24:01 +0800

    执行 TransH 示例

--- examples/TransH/single_gpu_transh_FB15K237_hpo.py
+++ examples/TransH/single_gpu_transh_FB15K237_hpo.py
@@ -160,11 +160,7 @@ print()
 start_hpo_train(config=sweep_config, count=3)
 
 ######################################################################
-# .. figure:: /_static/images/examples/TransH/TransH-FB15K237-HPO.png
-#      :align: center
-#      :height: 300
-#
-#      超参数优化结果
+# .. Note:: 上述代码的运行报告可以从 `此处 </zh-cn/latest/_static/pdfs/examples/TransH/TransH超参数搜索示例（一）.pdf>`_ 下载。
 
 ######################################################################
 # --------------
