55ec2bcb031bb3aad53538fcfda926645c2de682
LuYF-Lemon-love <3555028709@qq.com>
2024-05-14 14:17:08 +0800

    执行 TransD 示例

--- examples/TransD/single_gpu_transd_FB15K237_hpo.py
+++ examples/TransD/single_gpu_transd_FB15K237_hpo.py
@@ -5,6 +5,12 @@
 TransD-FB15K237-single-gpu-hpo
 ====================================================================
 
+.. Note:: created by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 7, 2023
+
+.. Note:: updated by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 14, 2024
+
+.. Note:: last run by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 14, 2024
+
 这一部分介绍如何用一个 GPU 在 ``FB15K237`` 知识图谱上寻找 ``TransD`` :cite:`TransD` 的超参数。
 
 定义训练数据加载器超参数优化范围
