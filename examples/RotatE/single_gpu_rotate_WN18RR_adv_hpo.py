fceb74442c2385e2a9104cb19a08ca680dcf6db5
LuYF-Lemon-love <3555028709@qq.com>
2024-05-15 20:59:40 +0800

    执行 TransD 示例

--- examples/RotatE/single_gpu_rotate_WN18RR_adv_hpo.py
+++ examples/RotatE/single_gpu_rotate_WN18RR_adv_hpo.py
@@ -6,6 +6,12 @@
 RotatE-WN18RR-single-gpu-adv-hpo
 ====================================================================
 
+.. Note:: created by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 7, 2023
+
+.. Note:: updated by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 15, 2024
+
+.. Note:: last run by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 15, 2024
+
 这一部分介绍如何用一个 GPU 在 ``WN18RR`` 知识图谱上寻找 ``RotatE`` :cite:`RotatE` 的超参数。
 
 定义训练数据加载器超参数优化范围
