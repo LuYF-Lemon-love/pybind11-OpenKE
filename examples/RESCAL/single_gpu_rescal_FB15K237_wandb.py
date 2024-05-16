740d02a622118f3237095b59ab36e729f6e39ee4
LuYF-Lemon-love <3555028709@qq.com>
2024-05-16 15:32:43 +0800

    执行 RESCAL 示例

--- examples/RESCAL/single_gpu_rescal_FB15K237_wandb.py
+++ examples/RESCAL/single_gpu_rescal_FB15K237_wandb.py
@@ -6,6 +6,12 @@
 RESCAL-FB15K237-single-gpu-wandb
 ====================================================================
 
+.. Note:: created by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 7, 2023
+
+.. Note:: updated by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 16, 2024
+
+.. Note:: last run by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 16, 2024
+
 这一部分介绍如何用一个 GPU 在 ``FB15K237`` 知识图谱上训练 ``RESCAL`` :cite:`RESCAL`，使用 ``wandb`` 记录实验结果。
 
 导入数据
@@ -35,6 +41,7 @@ wandb_logger = WandbLogger(
 		num_workers = 16,
 		dim = 50,
 		margin = 1.0,
+        use_tqdm = False,
 		use_gpu = True,
 		device = 'cuda:0',
 		epochs = 1000,
@@ -50,7 +57,7 @@ wandb_logger = WandbLogger(
 config = wandb_logger.config
 
 ######################################################################
-# pybind11-KE 提供了很多数据集，它们很多都是 KGE 原论文发表时附带的数据集。
+# pybind11-OpenKE 提供了很多数据集，它们很多都是 KGE 原论文发表时附带的数据集。
 # :py:class:`pybind11_ke.data.KGEDataLoader` 包含 ``in_path`` 用于传递数据集目录。
 
 # dataloader for training
@@ -113,7 +120,8 @@ model = NegativeSampling(
 # 使得训练器能够在训练过程中评估模型。
 
 # test the model
-tester = Tester(model = rescal, data_loader = dataloader, use_gpu = config.use_gpu, device = config.device)
+tester = Tester(model = rescal, data_loader = dataloader, use_tqdm = config.use_tqdm,
+                use_gpu = config.use_gpu, device = config.device)
 
 # train the model
 trainer = Trainer(model = model, data_loader = dataloader.train_dataloader(), epochs = config.epochs,
