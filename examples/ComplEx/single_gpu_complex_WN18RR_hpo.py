cb6e1ae1c71a44b2f85f3993f10349cda1bbaf9b
LuYF-Lemon-love <3555028709@qq.com>
2024-05-19 16:16:21 +0800

    执行 ComplEx 示例

--- examples/ComplEx/single_gpu_complex_WN18RR_hpo.py
+++ examples/ComplEx/single_gpu_complex_WN18RR_hpo.py
@@ -166,11 +166,8 @@ print()
 start_hpo_train(config=sweep_config, count=3)
 
 ######################################################################
-# .. figure:: /_static/images/examples/ComplEx/ComplEx-WN18RR-HPO.png
-#      :align: center
-#      :height: 300
-#
-#      超参数优化结果
+# .. Note:: 上述代码的运行日志可以从 `此处 </zh-cn/latest/_static/logs/examples/ComplEx/single_gpu_complex_WN18RR_hpo.txt>`_ 下载。
+# .. Note:: 上述代码的运行报告可以从 `此处 </zh-cn/latest/_static/pdfs/examples/ComplEx/ComplEx超参数搜索示例（一）.pdf>`_ 下载。
 
 ######################################################################
 # --------------
