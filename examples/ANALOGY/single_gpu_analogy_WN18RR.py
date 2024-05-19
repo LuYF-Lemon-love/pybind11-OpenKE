97d805e6a69da0453f0f9f298726bf6689fa7ed7
LuYF-Lemon-love <3555028709@qq.com>
2024-05-19 16:24:09 +0800

    执行 ANALOGY 示例

--- examples/ANALOGY/single_gpu_analogy_WN18RR.py
+++ examples/ANALOGY/single_gpu_analogy_WN18RR.py
@@ -6,6 +6,12 @@
 ANALOGY-WN18RR-single-gpu
 ====================================================================
 
+.. Note:: created by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 7, 2023
+
+.. Note:: updated by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 19, 2024
+
+.. Note:: last run by LuYF-Lemon-love <luyanfeng_nlp@qq.com> on May 19, 2024
+
 这一部分介绍如何用一个 GPU 在 ``WN18RR`` 知识图谱上训练 ``ANALOGY`` :cite:`ANALOGY`。
 
 导入数据
@@ -13,14 +19,14 @@ ANALOGY-WN18RR-single-gpu
 pybind11-OpenKE 有 1 个工具用于导入数据: :py:class:`pybind11_ke.data.TrainDataLoader`。
 """
 
-from pybind11_ke.data import KGEDataLoader, BernSampler, TradTestSampler
+from pybind11_ke.data import KGEDataLoader
 from pybind11_ke.module.model import Analogy
 from pybind11_ke.module.loss import SoftplusLoss
 from pybind11_ke.module.strategy import NegativeSampling
 from pybind11_ke.config import Trainer, Tester
 
 ######################################################################
-# pybind11-KE 提供了很多数据集，它们很多都是 KGE 原论文发表时附带的数据集。
+# pybind11-OpenKE 提供了很多数据集，它们很多都是 KGE 原论文发表时附带的数据集。
 # :py:class:`pybind11_ke.data.KGEDataLoader` 包含 ``in_path`` 用于传递数据集目录。
 
 # dataloader for training
@@ -82,11 +88,12 @@ model = NegativeSampling(
 # 使得训练器能够在训练过程中评估模型。
 	
 # test the model
-tester = Tester(model = analogy, data_loader = dataloader, use_gpu = True, device = 'cuda:1')
+tester = Tester(model = analogy, data_loader = dataloader, use_tqdm = False,
+                use_gpu = True, device = 'cuda:0')
 
 # train the model
 trainer = Trainer(model = model, data_loader = dataloader.train_dataloader(),
-	epochs = 2000, lr = 0.5, opt_method = "adagrad", use_gpu = True, device = 'cuda:1',
+	epochs = 2000, lr = 0.5, opt_method = "adagrad", use_gpu = True, device = 'cuda:0',
 	tester = tester, test = True, valid_interval = 100,
 	log_interval = 100, save_interval = 100,
 	save_path = '../../checkpoint/analogy.pth', delta = 0.01)
