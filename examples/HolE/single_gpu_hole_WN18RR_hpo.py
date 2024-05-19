8e513660277cb8098f0dcdf7ade0e568110cf15f
LuYF-Lemon-love <3555028709@qq.com>
2024-05-19 12:44:26 +0800

    执行 HolE 示例

--- examples/HolE/single_gpu_hole_WN18RR_hpo.py
+++ examples/HolE/single_gpu_hole_WN18RR_hpo.py
@@ -157,11 +157,8 @@ print()
 start_hpo_train(config=sweep_config, count=3)
 
 ######################################################################
-# .. figure:: /_static/images/examples/HolE/HolE-WN18RR-HPO.png
-#      :align: center
-#      :height: 300
-#
-#      超参数优化结果
+# .. Note:: 上述代码的运行日志可以从 `此处 </zh-cn/latest/_static/logs/examples/HolE/single_gpu_hole_WN18RR_hpo.txt>`_ 下载。
+# .. Note:: 上述代码的运行报告可以从 `此处 </zh-cn/latest/_static/pdfs/examples/HolE/HolE超参数搜索示例（一）.pdf>`_ 下载。
 
 ######################################################################
 # --------------
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m "执行 DistMult 示例"
+git commit -m "执行 HolE 示例"
 git push
