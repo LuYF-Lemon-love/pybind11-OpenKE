2cef7c9d525f7b0286d0da20b12636620802f11e
LuYF-Lemon-love <3555028709@qq.com>
2024-05-22 22:50:21 +0800

    执行 CompGCN 示例

--- examples/CompGCN/single_gpu_compgcn_FB15K237.py
+++ examples/CompGCN/single_gpu_compgcn_FB15K237.py
@@ -101,3 +101,10 @@ trainer = Trainer(model = model, data_loader = dataloader.train_dataloader(),
 	save_interval = 50, save_path = '../../checkpoint/compgcn.pth'
 )
 trainer.run()
+
+######################################################################
+# .. Note:: 上述代码的运行日志可以从 `此处 </zh-cn/latest/_static/logs/examples/CompGCN/single_gpu_compgcn_FB15K237.txt>`_ 下载。
+
+######################################################################
+# --------------
+#
