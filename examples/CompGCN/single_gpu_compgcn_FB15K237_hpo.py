76b6b684faa99395849fff017ffade9ca51d2f8e
LuYF-Lemon-love <3555028709@qq.com>
2024-05-24 15:19:21 +0800

    执行 CompGCN 示例

--- examples/CompGCN/single_gpu_compgcn_FB15K237_hpo.py
+++ examples/CompGCN/single_gpu_compgcn_FB15K237_hpo.py
@@ -184,11 +184,8 @@ print()
 start_hpo_train(config=sweep_config, count = 3)
 
 ######################################################################
-# .. figure:: /_static/images/examples/CompGCN/CompGCN-FB15K237-HPO.png
-#      :align: center
-#      :height: 300
-#
-#      超参数优化结果
+# .. Note:: 上述代码的运行日志可以从 `此处 </zh-cn/latest/_static/logs/examples/CompGCN/single_gpu_compgcn_FB15K237_hpo.txt>`_ 下载。
+# .. Note:: 上述代码的运行报告可以从 `此处 </zh-cn/latest/_static/pdfs/examples/CompGCN/CompGCN超参数搜索示例（一）.pdf>`_ 下载。
 
 ######################################################################
 # --------------
